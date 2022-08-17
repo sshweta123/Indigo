a4185cd59318ead00777f47d373bf1781e4ded21
thakur6 <pravinthakur666@gmail.com>
2022-08-17 19:54:59 +0530

    commit by pravin

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+	@BeforeSuite
+	public void init() {
+	
+	}
+	
+	@AfterSuite
+	public void TearDown() {
+		
+	}
+}
