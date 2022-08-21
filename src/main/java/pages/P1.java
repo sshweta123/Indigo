59d9a4cf6bb4186bbc06a638adf09d1c76e40d95
sshweta123 <sshwetasabne55@gmail.com>
2022-08-21 11:00:11 +0530

    commit by shweta1

--- src/main/java/pages/P1.java
+++ src/main/java/pages/P1.java
@@ -1,5 +1,40 @@
 package pages;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class P1 {
+	@FindBy(xpath="//*[@id='main']//*[@class='menu-top-container']//child::ul//li[1]")
+	private WebElement Demosbutton;
+	
+	@FindBy(xpath="//*[@id='main']//*[@class='menu-top-container']//child::ul//li//a[text()='About']")
+	private WebElement Aboutbutton;
+	
+		
+	//constructor
+	
+	public P1(WebDriver driver) {
+		
+		
+		PageFactory.initElements(driver, this);
+	}
+	
+	//action
+	
+	public void dmsbtn() {
+		
+		Demosbutton.click();		
+		
+	}
+	
+	
+	public void abtbtn() {
+		
+		Aboutbutton.click();
+	}
+	
 
 }
