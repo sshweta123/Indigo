a378fbbcccff4289a1661dea9467c193f5aaa63b
sudhir3011 <sudhirmane301191@gmail.com>
2022-08-21 10:27:54 +0530

    commit by sudhir 1

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,17 +1,32 @@
 package tests;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
 
+	public static WebDriver driver;
+	
 	@BeforeSuite
 	public void init() {
+		
+	WebDriverManager.chromedriver().setup();
 	
+	driver.manage().window().maximize();
+	
+	driver.get("https://jqueryui.com/");
+	
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
 	}
 	
 	@AfterSuite
 	public void TearDown() {
-		
+		driver.close();
 	}
 }
