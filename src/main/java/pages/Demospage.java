01b60b35b5204fd98083bd3a15a76c1a66d5f10e
thakur6 <pravinthakur666@gmail.com>
2022-08-21 11:43:33 +0530

    commit by pravin2

--- src/main/java/pages/Demospage.java
+++ src/main/java/pages/Demospage.java
@@ -1,13 +1,20 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Demospage {
   
     @FindBy(xpath="//*[@id='content']//child::ul//li[1]")
     private WebElement Dragblelink;
     
+public Demospage (WebDriver driver) {
+		
+		
+		PageFactory.initElements(driver, this);
+	}
     public void drop() {
     	Dragblelink.click();
     }
