f7e88b4221f90e01a478edc788a8a3edd80a8749
thakur6 <pravinthakur666@gmail.com>
2022-08-22 10:14:17 +0530

    commit by pravin3

--- src/main/java/pages/Demospage.java
+++ src/main/java/pages/Demospage.java
@@ -9,17 +9,25 @@ public class Demospage {
   
     @FindBy(xpath="//*[@id='content']//child::ul//li//a[text()='Draggable']")
     private WebElement Dragblelink;
-    
+    @FindBy(xpath="//*[@id='main']//child::ul//li//a[text()='Download']")
+    private WebElement Downloadlink;
 public Demospage (WebDriver driver) {
-		
 		
 		PageFactory.initElements(driver, this);
 	}
+
     public void drop() throws InterruptedException {
     	Thread.sleep(2000);
     	Dragblelink.click();
     	Thread.sleep(2000);
     }
+   
     
+public void down() throws InterruptedException {
+	Thread.sleep(2000);
+	Downloadlink.click();
+	Thread.sleep(2000);
+}
+
     
 }
